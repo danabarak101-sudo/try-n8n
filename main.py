441cf70a08b449d61a684005b5be5216b8d8cf38
danabarak101-sudo <danabarak101@gmail.com>
2026-03-19 22:22:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-a = 5
-b = 7
-print(f"a + b = {a + b}")
-print(f"a - b = {a - b}")
-print(f"a * b = {a * b}")
+def add(num1, num2):
+  return num1 + num2
+def sub(num1, num2):
+  return num1 - num2
+def multiple(num1, num2):
+  return num1 * num2
+
+print(add(5, 2))
+print(sub(5, 2))
+print(multiple(5, 2))
