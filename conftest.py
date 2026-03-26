17937ce47744450e12bbafbf631f66f7aa12b8dd
danabarak101-sudo <danabarak101@gmail.com>
2026-03-26 10:21:07 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 def pytest_addoption(parser):
-    parser.addoption("--machine_name", action="store")
-    parser.addoption("--version", action="store")
-    parser.addoption("--user", action="store")
-    parser.addoption("--password", action="store")
+    parser.addoption("--mname", action="store", help="machine name")
+    parser.addoption("--mver", action="store", help="version")
+    parser.addoption("--muser", action="store", help="user")
+    parser.addoption("--mpass", action="store", help="password")
