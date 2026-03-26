21494f97d168e94f6b4fb71f4bcbdc01d2a636bc
danabarak101-sudo <danabarak101@gmail.com>
2026-03-26 10:20:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,21 +11,26 @@ def sub(num1, num2):
 def multiple(num1, num2):
     return num1 * num2
 
-def parse_args():
+def args():
     # הגדרת ה-Parser לקבלת הפרמטרים מה-n8n
     parser = argparse.ArgumentParser(description="Run automation tests")
     
-    parser.add_argument("-m", "--machine_name", required=True, help="Machine name")
-    parser.add_argument("-v", "--version", required=True, help="Version")
-    parser.add_argument("-u", "--user", required=True, help="Username")
-    parser.add_argument("-p", "--password", required=True, help="Password")
+    parser.add_argument("-m", "--mname", required=True, help="Machine name")
+    parser.add_argument("-v", "--mver", required=True, help="Version")
+    parser.add_argument("-u", "--muser", required=True, help="Username")
+    parser.add_argument("-p", "--mpass", required=True, help="Password")
     
     return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parse_args()
-    command_str = f"python3 -m test_func.py -m {args.machine} -v {args.version} --user {args.user} --p {args.password}"
-    
+    args = args()
+    command_str = (
+            f"python -m pytest test_func.py -vs "
+            f"--mname {args.machine_name} "
+            f"--mver {args.version} "
+            f"--muser {args.user} "
+            f"--mpass {args.password}"
+        )
     print(f"Executing command: {command_str}")
     
     result = subprocess.run(
